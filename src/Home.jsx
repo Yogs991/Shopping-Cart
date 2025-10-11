7bba91b79fe5832be842975d43a2fcf2378726ea
Yog99 <124916155+Yogs991@users.noreply.github.com>
2025-10-11 15:02:46 +0300

    Basic layout in Home.jsx File

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./Navbar";
+
+const Home = ()=>{
+    return(
+        <>
+            <div className="home-header">
+                <h2>TOPShop</h2>
+                <NavBar />
+            </div>
+            <div className="home-main">
+                <div>
+                    <h1>Discover Quality Products for Everyday Life</h1>
+                    <p>Explore our collection of carefully curated items designed to enhance your lifestyle.</p>
+                    <button>Shop Now</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+// import {Link} from 'react-router';
+
+const NavBar = ()=>{
+    return(
+        <>
+            <div className='navbar'>
+                {/* <Link to="/">Home</Link>
+                <Link to="/">Shop</Link>
+                <Link to="/">Cart</Link> */}
+                <button>Home</button>
+                <button>Shop</button>
+                <button>Cart</button>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
