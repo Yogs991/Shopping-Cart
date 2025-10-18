d249d65effff9457c0dd29fad0a5fb8b0af5393e
Yog99 <124916155+Yogs991@users.noreply.github.com>
2025-10-18 13:36:52 +0300

    Changes to css and finished logic of Shop.jsx

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -5,6 +5,7 @@ const Shop = ()=>{
     const [cards, setCard] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [quantities, setQuantities] = useState({});
 
     useEffect(()=>{
         fetch("https://fakestoreapi.com/products",{mode: "cors"})
@@ -14,13 +15,44 @@ const Shop = ()=>{
                 }
                 return response.json();
             })
-            .then((response)=>setCard(response))
+            .then((response)=>{
+                setCard(response);
+                const initialQuantities = {};
+                response.forEach((item)=>{
+                    initialQuantities[item.id] =1;
+                });
+                setQuantities(initialQuantities);
+            })
             .catch((error)=> setError(error))
             .finally(()=>setLoading(false));
+            
     },[]);
 
     if(loading) return <p>Loading...</p>
     if(error) return <p>A network error was encountered</p>
+
+    const increaseQuantity = (id)=>{
+        setQuantities((prev)=>({
+            ...prev,
+            [id]: prev[id] + 1,
+        }));
+    };
+
+    const decreaseQuantity = (id)=>{
+        setQuantities((prev)=>({
+            ...prev,
+            [id]: prev[id]-1,
+        }));
+    };
+    
+    const handleInputChange = (id, value)=>{
+        const number = parseInt(value, 10);
+        setQuantities((prev)=>({
+            ...prev,
+            [id]: number,
+        }));
+    }
+
 
     return(
         <>
@@ -32,8 +64,18 @@ const Shop = ()=>{
                 <div className="card-list">
                     {cards.map((card)=>(
                         <div key={card.id} className="card">
-                            <h2>{card.title}</h2>
-                            <img src={card.image} alt={card.title} />
+                            <div>
+                                <h2>{card.title}</h2>
+                            </div>
+                            <div>
+                                <img src={card.image} alt={card.title} />
+                            </div>
+                            <div className="buttons-list">
+                                <button className="buttons" onClick={()=>decreaseQuantity(card.id)}>-</button>
+                                <input type="number" value={quantities[card.id] || 1} onChange={(e)=>handleInputChange(card.id,e.target.value)}/>
+                                <button className="buttons" onClick={()=>increaseQuantity(card.id)}>+</button>
+                                <button className="buttons">Add to cart</button>
+                            </div>
                         </div>
                     ))}
                 </div>
